183c504eb3ff4d85a5e73b913acf9d48f5f9b10e
SebastianFlores <berendicto@gmail.com>
2023-08-17 22:59:41 -0400

    Modificaciones a los estilos y agregando las otras 3 cards

--- adoptaunperrito/src/App.jsx
+++ adoptaunperrito/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './components/header'
 import Card from './components/Card'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -18,8 +17,29 @@ function App() {
         tagTexto="Chocolito"
         tagColor="primary"
       />
-
+      <Card
+        imageUrl="https://plus.unsplash.com/premium_photo-1669040674572-836c64a3ac70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80"
+        nombre="Perro Putazo"
+        descripcion="Perro que usara su ternura para sacarte hasta el ultimo peso!."
+        tagTexto="Coolberto"
+        tagColor="secondary"
+      />
+      <Card
+        imageUrl="https://images.unsplash.com/photo-1529467037979-99d067b7677d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
+        nombre="Perro Trolo"
+        descripcion="Monta todo lo que encuentra."
+        tagTexto="Sin respeto"
+        tagColor="danger"
+      />
+      <Card
+        imageUrl="https://images.unsplash.com/photo-1562714529-94d65989df68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80"
+        nombre="Amenaza Fantasma"
+        descripcion="No dejes que su cara de -pendejito- te distraiga de sus planes."
+        tagTexto="Furia"
+        tagColor="info"
+      />
       </div>
+      <Footer/>
     </>
   )
 }
---
+++ adoptaunperrito/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <h3>Perritos ricolinos que robaran tu cucharon!</h3>
+  )
+}
