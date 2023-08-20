0a618acca653f7a7d116cdefe4cbffb10ce6c7c4
SebastianFlores <berendicto@gmail.com>
2023-08-20 18:02:17 -0400

    Modificaciones renderizado a traves de un array y mapeando

--- adoptaunperrito/src/App.jsx
+++ adoptaunperrito/src/App.jsx
@@ -1,47 +1,57 @@
+import "./App.css";
+import Header from "./components/header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 
-import './App.css'
-import Header from './components/header'
-import Card from './components/Card'
-import Footer from './components/Footer'
+const perros = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
+    nombre: "Perro Chocolo",
+    descripcion: "Perro chocolo que roba tu corasound.",
+    tagTexto: "Chocolito",
+    tagColor: "primary",
+  },
+  {
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1669040674572-836c64a3ac70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80",
+    nombre: "Perro Putazo",
+    descripcion:
+      "Perro que usara su ternura para sacarte hasta el ultimo peso!.",
+    tagTexto: "Coolberto",
+    tagColor: "secondary",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1529467037979-99d067b7677d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+    nombre: "Perro Trolo",
+    descripcion: "Monta todo lo que encuentra.",
+    tagTexto: "Sin respeto",
+    tagColor: "danger",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1562714529-94d65989df68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80",
+    nombre: "Amenaza Fantasma",
+    descripcion:
+      "No dejes que su cara de -pendejito- te distraiga de sus planes.",
+    tagTexto: "Furia",
+    tagColor: "info",
+  },
+];
 
 function App() {
-
   return (
     <>
-      <Header titulo="Adopta un perrito"/>
+      <Header titulo="Adopta un perrito" />
       <div className="App">
-      <Card
-        imageUrl="https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-        nombre="Perro Chocolo"
-        descripcion="Perro chocolo que roba tu corasound."
-        tagTexto="Chocolito"
-        tagColor="primary"
-      />
-      <Card
-        imageUrl="https://plus.unsplash.com/premium_photo-1669040674572-836c64a3ac70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80"
-        nombre="Perro Putazo"
-        descripcion="Perro que usara su ternura para sacarte hasta el ultimo peso!."
-        tagTexto="Coolberto"
-        tagColor="secondary"
-      />
-      <Card
-        imageUrl="https://images.unsplash.com/photo-1529467037979-99d067b7677d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
-        nombre="Perro Trolo"
-        descripcion="Monta todo lo que encuentra."
-        tagTexto="Sin respeto"
-        tagColor="danger"
-      />
-      <Card
-        imageUrl="https://images.unsplash.com/photo-1562714529-94d65989df68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80"
-        nombre="Amenaza Fantasma"
-        descripcion="No dejes que su cara de -pendejito- te distraiga de sus planes."
-        tagTexto="Furia"
-        tagColor="info"
-      />
+        {perros.map((perro, index) => (
+          <Card key={index} {...perro} />
+        ))}
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- adoptaunperrito/src/components/Card.jsx
+++ adoptaunperrito/src/components/Card.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import Tags from './Tags';
 
 const Card = (props) => {
-    const { imageUrl, nombre, descripcion, tagTexto, tagColor } = props;
-
     return (
         <div className="card">
-            <img src={imageUrl} className="card-image" />
-            <h2 className="card-name">{nombre}</h2>
-            <p className="card-description">{descripcion}</p>
-            <Tags texto={tagTexto} backgroundColor={tagColor} />
-        </div>
-    );
+        <img src={props.imageUrl} className="card-image" />
+        <h2 className="card-name">{props.nombre}</h2>
+        <p className="card-description">{props.descripcion}</p>
+        <Tags texto={props.tagTexto} backgroundColor={props.tagColor} />
+    </div>
+);
 };
 
 export default Card;
