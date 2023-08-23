a84f21668499f57c7396b31ad02f79ae0d6853b2
SebastianFlores <berendicto@gmail.com>
2023-08-22 22:02:57 -0400

    Render correcto del header

--- adoptaunperrito/src/App.jsx
+++ adoptaunperrito/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
 
